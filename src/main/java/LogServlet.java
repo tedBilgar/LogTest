a7c952810eae439790603ea963a9c28e133b2b4e
tedBilgar <mcquicklyss@gmail.com>
2018-08-02 21:54:24 +0500

    Log4j LOGs

--- src/main/java/LogServlet.java
+++ src/main/java/LogServlet.java
@@ -1,20 +1,39 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Logger;
+
 
 public class LogServlet extends HttpServlet {
-    private static final Logger log = Logger.getLogger(LogServlet.class.getName());
+    //private static final Logger log = Logger.getLogger(LogServlet.class);
+    private Logger logger = null;
     @Override
     public void init() throws ServletException {
-        Timer t = new Timer( );
+        //final Logger log = Logger.getLogger(LogServlet.class);
+        //BasicConfigurator.configure();
+        logger = Logger.getRootLogger();
+        /*Timer t = new Timer( );
         t.scheduleAtFixedRate(new TimerTask() {
 
             @Override
             public void run() {
                 //System.out.println("Hello world");
-                log.info("Hello World");
+                //log.info("Hello World");
+            }
+        }, 1000,1000);*/
+        printHello();
+    }
+
+    public void printHello(){
+        Timer t = new Timer( );
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                logger.info("Hello world");
             }
         }, 1000,1000);
     }
